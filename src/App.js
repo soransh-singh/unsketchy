66182dfe2cded2cf227b6b2b77ae1b5803eb7b02
soransh <57911107+soransh-singh@users.noreply.github.com>
2021-12-14 11:44:53 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import {useState, useEffect} from 'react'
 
 function App() {
-  
+  // use state for form
+  // use state for timer
+  // use state to maintain boolean
+
+  // useEffect to fetch the image from api
+
   return (
-    <div className="App">
-      hello world
-    </div>
+    <main>
+
+      <div>
+        {/*
+          select to choose time //handle change
+          select to choose type of img //handle change
+          submit button //handle submit
+        */}
+      </div>
+      {/*
+        we need conditional rendring so whever it shows image form is hidden
+      */}
+    </main>
   );
 }
 
